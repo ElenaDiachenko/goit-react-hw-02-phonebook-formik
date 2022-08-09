a253ae0d9b4d077b159aa47ff8dc0192a966fbb4
ElenaDiachenko <96205320+ElenaDiachenko@users.noreply.github.com>
2022-08-09 22:20:39 +0300

    added compoment Contact

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,19 +1,17 @@
 import PropTypes from 'prop-types';
 import { Box } from '../Box';
-import {
-  ContactItem,
-  ContactName,
-  ContactNumber,
-  DeleteButton,
-} from './ContactList.styled';
+import { ContactItem } from './ContactList.styled';
+import { Contact } from 'components/Contact/Contact';
 
 export const ContactList = ({ contacts, onDeleteContact }) => (
   <Box as="ul" display="flex" flexDirection="column" gridGap={3}>
     {contacts.map(({ id, name, number }) => (
-      <ContactItem key={id} name={name}>
-        <ContactName>{name}: </ContactName>
-        <ContactNumber>{number}</ContactNumber>
-        <DeleteButton onClick={() => onDeleteContact(id)}>Delete</DeleteButton>
+      <ContactItem key={id}>
+        <Contact
+          name={name}
+          number={number}
+          onDelete={() => onDeleteContact(id)}
+        />
       </ContactItem>
     ))}
   </Box>
